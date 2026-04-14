5cd9c04f9085017fd77312575e06298a2c29b61b
Gamaliel Gonzalez <gamas_05@live.com.mx>
2026-04-13 21:34:41 -0600

    se actualizo js

--- js/variables.js
+++ js/variables.js
@@ -2,7 +2,10 @@ let num1 = 10;
 let num2 = 20;
 
 const pi = 3.1415;
-//este es un comentario
-resultado = num1 + num2;
+// este es un comentario
 
-print(resultado);
+// Es buena práctica usar 'let' o 'const' para declarar variables
+let resultado = num1 + num2;
+
+// En JavaScript para terminal se usa console.log()
+console.log(resultado);
