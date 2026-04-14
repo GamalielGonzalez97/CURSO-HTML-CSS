2106fa81da4ff589e36ea9e853b4aacb2b7c106e
Gamaliel Gonzalez <gamas_05@live.com.mx>
2026-04-13 21:41:48 -0600

    se agrego js en version2 y une a la rama main

---
+++ js/variables.js
@@ -0,0 +1,13 @@
+let num1 = 10;
+let num2 = 20;
+
+const pi = 3.1415;
+// este es un comentario
+
+// Es buena práctica usar 'let' o 'const' para declarar variables
+let resultado = num1 + num2;
+
+// En JavaScript para terminal se usa console.log()
+console.log(resultado);
+
+//se  copio variables.js para version2
