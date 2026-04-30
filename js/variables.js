ed4976176d6f0be63e07b609227ab9ce7b73b051
Gamaliel Gonzalez <gamas_05@live.com.mx>
2026-04-29 22:42:30 -0600

    actualizar js

--- js/variables.js
+++ js/variables.js
@@ -1,11 +1,2 @@
-let num1 = 10;
-let num2 = 20;
-
-const pi = 3.1415;
-// este es un comentario
-
-// Es buena práctica usar 'let' o 'const' para declarar variables
-let resultado = num1 + num2;
-
-// En JavaScript para terminal se usa console.log()
-console.log(resultado);
+const nombre = "Nacho";
+console.log(nombre); // <--- Sin esto, la terminal no muestra nada
