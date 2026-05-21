4589b5a5c2ea23cc49f10fd39d93e00afa295ec8
Gamaliel Gonzalez <gamas_05@live.com.mx>
2026-05-20 22:12:27 -0600

    se agrega archivo for.js

---
+++ js/For.js
@@ -0,0 +1,9 @@
+// Empieza en la silla 1, avanza de 1 en 1, hasta llegar a la 3
+for (let silla = 1; silla <= 3; silla++) {
+  console.log("Limpiando la silla número " + silla);
+}
+
+// Resultado en la consola:
+// Limpiando la silla número 1
+// Limpiando la silla número 2
+// Limpiando la silla número 3
