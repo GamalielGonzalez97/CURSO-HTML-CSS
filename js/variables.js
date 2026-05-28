b26fc07338468e1550d70b39d0c32c13eb418781
Gamaliel Gonzalez <gamas_05@live.com.mx>
2026-05-27 21:25:01 -0600

    actualizar variables

--- js/variables.js
+++ js/variables.js
@@ -1,63 +1,11 @@
-const nombre = "Gamaliel";
-console.log(nombre); // <--- Sin esto, la terminal no muestra nada
+//let Se usa cuando el valor puede modificarse después.
+let nombre = "Gamaliel";
+console.log(nombre);
 
-let edad = 28;
-console.log(edad);
+nombre = "Juan";
+console.log(nombre);
 
-var a = 28;
-var b = 12;
+//const se usa cuando el valor no puede modificarse después.
 
-var resultado = a + b;
-
-console.log(resultado);
-
-mayor = true;
-menor = false;
-console.log(mayor);
-
-let esMayorDeEdad = true;
-let tieneDescuento = false;
-
-// Object: Colección de propiedades (clave: valor)
-const usuario = {
-  nombre: "Ana",
-  puntos: 1500,
-  esPremium: true,
-};
-
-console.log(usuario);
-
-// Array: Lista de elementos (colección indexada)
-const colores = ["rojo", "verde", "azul"];
-console.log(colores[0]); // Imprime: "rojo"
-
-// Function: Bloques de código ejecutables
-const saludar = function () {
-  return "¡Hola Mundo!";
-};
-
-console.log(saludar);
-
-let textoNum = "42";
-
-// Opción A: Función Number() - La más limpia
-let num1 = Number(textoNum);
-
-// Opción B: parseInt() - Extrae el entero (ignora decimales)
-let num2 = parseInt("42.5"); // Resultado: 42
-
-// Opción C: El truco del operador unario (+)
-let num3 = +textoNum; // Forma rápida de convertir
-
-Arraysupermercado = ["manzana", "platano", "pera"];
-console.log(Arraysupermercado[2]);
-
-// Creación de la lista (Array)
-const listaDeTareas = [
-  "Comprar leche",
-  "Estudiar JavaScript",
-  "Ir al gimnasio",
-];
-
-// 1. Ver toda la lista completa
-console.log(listaDeTareas);
+const pais = "México";
+console.log(pais);
