fcf088d1e60ef2d8c6b733bdb2e389b2f9fca7b9
Gamaliel Gonzalez <gamas_05@live.com.mx>
2026-05-29 23:01:36 -0600

    array

---
+++ js/Ejercicio-array.js
@@ -0,0 +1,12 @@
+let videojuegos = ["Minecraft", "Fortnite", "GTA V"];
+
+console.log("Lista inicial:");
+console.log(videojuegos);
+
+videojuegos.push("Call of Duty");
+
+console.log("Después de agregar:");
+console.log(videojuegos);
+
+console.log("Primer juego:");
+console.log(videojuegos[0]);
