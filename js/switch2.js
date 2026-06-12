bd29fe8de38fdbbb3435e4e9efb2a1eb6e7c6147
Gamaliel Gonzalez <gamas_05@live.com.mx>
2026-06-11 22:58:28 -0600

    actualiza main pasa cambios de la rama version2

---
+++ js/switch2.js
@@ -0,0 +1,30 @@
+//importar herramienta de node.js para leer datos por consola
+const readline = require("readline");
+
+//configurar para la entrada y salida de datos por consola
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+//preguntar al usuario usuario por consola y se quedan esperando a que el usuario ingrese un dato y presione enter
+rl.question("escribe una fruta(manzana, platano, uva): ", (fruta) => {
+  //convertir el dato ingresado a un número entero
+
+  switch (fruta) {
+    case "manzana":
+      console.log("La manzana es roja");
+      break;
+    case "platano":
+      console.log("El platano es amarillo");
+      break;
+    case "uva":
+      console.log("La uva es morada");
+      break;
+    default:
+      console.log("Fruta no reconocida");
+  }
+
+  //cerrar la interfaz de lectura de datos por consola
+  rl.close();
+});
